10c82e71fa5f4cb35aecc6bba1b12bfdf5a0d661
Matias Bacelar <matiasbacelar@gmail.com>
2025-05-04 04:32:39 +0000

    wezterm configuration

---
+++ wezterm/Readme.md
@@ -0,0 +1,4 @@
+# Important
+
+- For the wezterm configuration, I used this video (https://www.youtube.com/watch?v=l5c5ucQvKOA) as a reference.
+- All the lua files are located in a .config/wezterm in my Windows.
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,61 @@
+-- Allow wezterm to understand windows paths 
+package.path = package.path
+  .. ";E:\\.config\\wezterm\\?.lua"
+  .. ";E:\\.config\\wezterm\\?/init.lua"
+
+local wezterm = require("wezterm")
+local constants = require("constants")
+local scripts = require("scripts")
+local config = wezterm.config_builder()
+
+-- Font
+config.font_size = 12
+config.line_height = 1.2
+config.font = wezterm.font("CaskaydiaCove Nerd Font")
+
+-- Colors
+config.colors = {
+ cursor_bg = "white",
+ cursor_border = "white"
+}
+config.color_scheme = "Oceanic Next (Gogh)"
+
+-- Appearance
+config.window_decorations = "RESIZE"
+config.window_padding = {
+ left = 10,
+ right = 10,
+ top = 0,
+ bottom = 0,
+}
+config.foreground_text_hsb = {
+  hue = 1.0,
+  saturation = 1.2,
+  brightness = 2,
+}
+config.background = {{
+ source = { File = { path = constants.bg_image }},
+ opacity = 1,
+ width = "100%",
+ hsb = { brightness = 0.25 },
+}}
+
+-- config.hide_tab_bar_if_only_one_tab = true
+
+-- The blur effect in windows is not working
+--[[
+config.window_background_opacity = 0
+config.win32_system_backdrop = "Acrylic"
+]]
+
+-- Miscellaneous
+config.mouse_bindings = constants.mouse_bindings
+config.disable_default_key_bindings = true
+config.keys = constants.key_bindings
+config.max_fps = 120
+config.prefer_egl = true
+
+-- WSL settings
+config.default_prog = { "wsl.exe", "~", "--cd", "~" }
+
+return config
